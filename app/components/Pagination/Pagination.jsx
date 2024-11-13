620eb80e812d358dcc271ae8a6ce95f007b0032d
yuriivintoniak <you@example.com>
2024-11-13 17:51:26 +0200

    feat: add OMDb API for movie data and setup pagination

---
+++ app/components/Pagination/Pagination.jsx
@@ -0,0 +1,24 @@
+import { Link } from "@remix-run/react";
+import styles from "./Pagination.css?url";
+
+export default function Pagination({ currentPage, totalPages }) {
+  return (
+    <div className="pagination">
+      {currentPage > 1 && (
+        <Link to={`?page=${Number(currentPage) - 1}`}>
+          <span>&lt;</span>
+        </Link>
+      )}
+      <p>{currentPage} of {totalPages}</p>
+      {currentPage < totalPages && (
+        <Link to={`?page=${Number(currentPage) + 1}`}>
+          <span>&gt;</span>
+        </Link>
+      )}
+    </div>
+  );
+}
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -2,38 +2,62 @@ import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import styles from "../styles/index.css?url";
 import MovieCard, { links as movieCardLinks } from "../components/MovieCard/MovieCard";
+import Pagination, { links as paginationLinks } from "../components/Pagination/Pagination";
 
 export const meta = () => [
   { title: "New Remix App" },
   { name: "description", content: "Welcome to Remix!" },
 ];
 
-export const loader = async () => {
-  const res = await fetch("https://freetestapi.com/api/v1/movies?limit=20");
-  return json(await res.json());
+export const loader = async ({ request }) => {
+  try {
+    const url = new URL(request.url);
+    const page = url.searchParams.get("page");
+    const res = await fetch(
+      `http://www.omdbapi.com/?s=movie&apikey=5ccd3d05&page=${page}`
+    );
+    const data = await res.json();
+
+    return json({
+      movies: data.Search,
+      totalResults: data.totalResults,
+      currentPage: page,
+    });
+  } catch (error) {
+    console.error(error);
+    throw new Response({
+      status: error.status,
+      statusText: error.statusText,
+    });
+  }
 }
 
 export default function Index() {
-  const movies = useLoaderData();
+  const { movies, totalResults, currentPage } = useLoaderData();
+  const totalPages = Math.ceil(totalResults / 10);
 
   return (
     <div>
       <ul className="movie-list">
         {movies.map((movie) => (
-          <li key={movie.id}>
+          <li key={movie.imdbID}>
             <MovieCard 
-              id={movie.id} 
-              title={movie.title} 
-              poster={movie.poster} 
+              id={movie.imdbID} 
+              title={movie.Title} 
+              poster={movie.Poster} 
             />
           </li>
         ))}
-      </ul> 
+      </ul>
+      <Pagination
+        currentPage={currentPage} totalPages={totalPages} 
+      />
     </div>
   );
 }
 
 export const links = () => [
   ...movieCardLinks(),
+  ...paginationLinks(),
   { rel: "stylesheet", href: styles },
 ];
